6f98bcf620358eedd8b90480dd62dbec339276ea
itzibtihal <ibtihalboukhanchouch@gmail.com>
2024-09-18 17:58:40 +0200

    add MaterialsNotFoundException

--- src/exceptions/MaterialsNotFoundException.java
+++ src/exceptions/MaterialsNotFoundException.java
@@ -1,5 +1,7 @@
 package exceptions;
 
+import java.sql.SQLException;
+
 public class MaterialsNotFoundException extends RuntimeException {
     public MaterialsNotFoundException() {
         super("Material not found");
@@ -7,5 +9,8 @@ public class MaterialsNotFoundException extends RuntimeException {
 
     public MaterialsNotFoundException(String message) {
         super(message);
+    }
+
+    public MaterialsNotFoundException(String errorFindingMaterial, SQLException e) {
     }
 }
